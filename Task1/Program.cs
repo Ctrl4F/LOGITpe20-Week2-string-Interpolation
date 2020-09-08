88a6d8212b1b504317b8b7768aabf5f341cce3da
Opilane TTHK <opilane@tthk.ee>
2020-09-08 12:24:29 +0300

    Week2

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName;
+            string LastName;
+            Console.WriteLine("What is your first name?");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            LastName = Console.ReadLine();
+            
+            Console.WriteLine("What is your year of birth?");
+            string UserInput;
+            UserInput = Console.ReadLine();
+            int YearOfBirth;
+            YearOfBirth = Int32.Parse(UserInput);
+            int Age = 2020 - YearOfBirth;
+            
+            Console.WriteLine($"Hello, {FirstName} {LastName} {Age}");
+            
+            
+
+        }
+    }
+}
